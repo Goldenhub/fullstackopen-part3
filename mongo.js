5b2c24ba65aaf337efccd4e4fab17088906cfad4
Golden <goldenazubuike@gmail.com>
2022-10-22 20:45:46 +0100

    connecting to mongodb atlas using mongoose ODM

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+if(process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>');
+  process.exit(1);
+}
+
+const password = encodeURIComponent(process.argv[2]);
+
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.yy4vaj2.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+const PhoneBookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Phonebook = mongoose.model('phonebook', PhoneBookSchema);
+
+
+mongoose.connect
+    .then(e => {
+        console.log('connected')
+        if (process.argv.length === 3) {
+            return Phonebook.find({});
+        }
+        const entry = Phonebook({
+            name: process.argv[3],
+            number: process.argv[4]
+        })
+        return entry.save();
+    })
+    .then(result => {
+        if (process.argv.length === 3) {
+            console.log('phonebook:');
+            result.forEach(entry => {
+                console.log(`${entry.name} ${entry.number}`);
+            })
+        }
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        return mongoose.connection.close();
+    })
+    .catch(err => {
+        console.log(err);
+    })
