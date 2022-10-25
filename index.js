064e681c6319197ca0f1fbdb49b0a8e5f58fe722
Golden <goldenazubuike@gmail.com>
2022-10-25 17:09:06 +0100

    added eslint for static analysis in Node App development build

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express');
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const express = require('express')
 require('dotenv').config()
-const cors = require('cors');
-const morgan = require('morgan');
-const app = express();
-app.use(express.json());
-const Phonebook = require('./models/phonebook.js');
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+app.use(express.json())
+const Phonebook = require('./models/phonebook.js')
 morgan.token('test', (req, res) => {
-    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 app.use(morgan(':method :status :res[content-length] - :response-time ms :test'))
 app.use(cors())
@@ -15,136 +18,136 @@ app.use(express.static('build'))
 
 // Homepage
 app.get('/', (request, response) => {
-    response.send('<h1>Welcome to the Persons DataBase</h1>');
+  response.send('<h1>Welcome to the Persons DataBase</h1>')
 })
 
 // Get all Persons
 app.get('/api/persons', (request, response) => {
-    Phonebook.find({}).then(entries => {
-        response.json({status: 200, message: "successful", data: entries})
-    })
+  Phonebook.find({}).then(entries => {
+    response.json({ status: 200, message: 'successful', data: entries })
+  })
 })
 
 // Get Info
 app.get('/info', (request, response, next) => {
-    const date = new Date();
-    Phonebook.find({})
-        .then(entries => {
-            response.send(
-                `<p>Phonebook has info for ${entries.length} entries</p>
+  const date = new Date()
+  Phonebook.find({})
+    .then(entries => {
+      response.send(
+        `<p>Phonebook has info for ${entries.length} entries</p>
                 <p>${date}</p>`
-            );
-        })
-        .catch(err => next(err))
+      )
+    })
+    .catch(err => next(err))
 })
 
 // Get a Person
 app.get('/api/persons/:id', (request, response, next) => {
-    let id = request.params.id;
-    Phonebook.findById(id)
-        .then(entry => {
-            if (entry) {
-                response.json({status: 200, message: "successful", data: entry})
-            } else {
-                response.statusMessage = "Resource Not Found";
-                response.status(404).json({status: 404, message: 'Resource Not Found'})
-            }
-        })
-        .catch(err => next(err))
+  let id = request.params.id
+  Phonebook.findById(id)
+    .then(entry => {
+      if (entry) {
+        response.json({ status: 200, message: 'successful', data: entry })
+      } else {
+        response.statusMessage = 'Resource Not Found'
+        response.status(404).json({ status: 404, message: 'Resource Not Found' })
+      }
+    })
+    .catch(err => next(err))
 
 })
 
 // Delete a Person
 app.delete('/api/persons/:id', (request, response, next) => {
-    let id = request.params.id;
+  let id = request.params.id
 
-    Phonebook.findByIdAndRemove(id)
-        .then(deletedRecord => {
-            console.log(deletedRecord);
-            if (deletedRecord) {
-                response.statusMessage = 'Entry deleted'
-                response.status(202).end();
-            }
-            else {
-                response.statusMessage = 'Resource not found'
-                response.status(404).end()
-            }
-        })
-        .catch(err => next(err))
+  Phonebook.findByIdAndRemove(id)
+    .then(deletedRecord => {
+      console.log(deletedRecord)
+      if (deletedRecord) {
+        response.statusMessage = 'Entry deleted'
+        response.status(202).end()
+      }
+      else {
+        response.statusMessage = 'Resource not found'
+        response.status(404).end()
+      }
+    })
+    .catch(err => next(err))
 })
 
 // Post a Person
 app.post('/api/persons', (request, response, next) => {
-    let {name, number} = request.body;
+  let { name, number } = request.body
 
-    const entry = new Phonebook({
-        name: name,
-        number: number
+  const entry = new Phonebook({
+    name: name,
+    number: number
+  })
+
+  let duplicate = undefined
+
+  Phonebook.findOne({ name: name })
+    .then(value => {
+      if (value) {
+        duplicate = true
+      }
+      return duplicate
     })
-
-    let duplicate = undefined;
-
-    Phonebook.findOne({ name: name })
-        .then(value => {
-            if (value) {
-                duplicate = true;
-            }
-            return duplicate;
-        })
-        .then(status => {
-            if (!status) {
-                entry.save()
-                    .then(savedEntry => {
-                        response.json({status: 200, message: "New entry added", data: savedEntry})
-                    })
-                    .catch(err => next(err))
-            } else {
-                response.json({status: 409, message: "Name already exist"})
-            }
-        })
+    .then(status => {
+      if (!status) {
+        entry.save()
+          .then(savedEntry => {
+            response.json({ status: 200, message: 'New entry added', data: savedEntry })
+          })
+          .catch(err => next(err))
+      } else {
+        response.json({ status: 409, message: 'Name already exist' })
+      }
+    })
         
 
 
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    let {name, number} = request.body;
-    let id = request.params.id;
-    const person = {
-        name: name,
-        number: number,
-    }
+  let { name, number } = request.body
+  let id = request.params.id
+  const person = {
+    name: name,
+    number: number,
+  }
     
-    Phonebook.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
-        .then(updatedRecord => {
-            response.json({status: 200, message: "Record updated", data: updatedRecord})
-        })
-        .catch(err => next(err))
+  Phonebook.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedRecord => {
+      response.json({ status: 200, message: 'Record updated', data: updatedRecord })
+    })
+    .catch(err => next(err))
 })
 
 // Catching unknown endpoints
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({status: 404, error: 'unknown endpoint' })
+  response.status(404).send({ status: 404, error: 'unknown endpoint' })
 }
   
 app.use(unknownEndpoint)
 
 // error handling
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message);
+  console.log(error.message)
 
-    if (error.name === 'CastError') {
-        response.json({ status: 400, error: 'malformatted ID' })
-    }else if(error.name === 'ValidationError') {
-        response.json({ status: 400, error: error.message })
-    }
+  if (error.name === 'CastError') {
+    response.json({ status: 400, error: 'malformatted ID' })
+  }else if(error.name === 'ValidationError') {
+    response.json({ status: 400, error: error.message })
+  }
 
-    next(error);
+  next(error)
 }
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || '3001'
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
--- models/phonebook.js
+++ models/phonebook.js
@@ -1,40 +1,43 @@
-const mongoose = require("mongoose");
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(response => {
-        console.log("connected to", url)
-    })
-    .catch(err => {
-        console.log(err.message)
-    })
+  .then(response => {
+    console.log('connected to', url)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
 
 const phonebookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 3
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: (value) => /(\d{2,})-(\d{6,})|(\d{3,})-(\d{5,})/.test(value),
-            message: props => `${props.value} is not a valid number`
-        }
+  name: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: (value) => /(\d{2,})-(\d{6,})|(\d{3,})-(\d{5,})/.test(value),
+      message: props => `${props.value} is not a valid number`
     }
+  }
 })
 
 phonebookSchema.set('toJSON', {
-    transform: (document, requestedObject) => {
-        requestedObject.id = requestedObject._id
-        delete requestedObject._id
-        delete requestedObject.__v
-    }
+  transform: (document, requestedObject) => {
+    requestedObject.id = requestedObject._id
+    delete requestedObject._id
+    delete requestedObject.__v
+  }
 })
 
 module.exports = mongoose.model('PhoneBook', phonebookSchema)
